f1ce409291ea24e2105c77f9672c8558c3342318
배성환 <baeseongwhan@ulim.co>
2025-05-07 11:13:57 +0900

    프로젝트 초기세팅

---
+++ src/main/java/agit/bgmagit/BgmagitApplication.java
@@ -0,0 +1,13 @@
+package agit.bgmagit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgmagitApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(BgmagitApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/agit/bgmagit/BgmagitApplicationTests.java
@@ -0,0 +1,13 @@
+package agit.bgmagit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgmagitApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
