caea68b0dbc112c7500765138d307c38f9870239
PeachCoolpis <121959157+PeachCoolpis@users.noreply.github.com>
2025-05-18 00:42:03 +0900

    Update WebConfig.java

--- src/main/java/agit/bgmagit/config/WebConfig.java
+++ src/main/java/agit/bgmagit/config/WebConfig.java
@@ -17,10 +17,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(corsUrl,corsUrl2)
-                .allowCredentials(true)
+                .allowedOrigins(corsUrl,"https://bgmagit.co.kr")
                 .allowedHeaders("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS");
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
     
     @Override
